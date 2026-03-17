4edf7e968abd54ce303695e8ecf1f1b502441845
Aliyu Sani <aliyu.alsajjadsani@vegaitglobal.com>
2026-03-17 10:21:07 +0100

    feat: inverted index

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -2,12 +2,55 @@
 
 import argparse
 import json
+import os
 import string 
+import pickle
 from nltk.stem import PorterStemmer
 
 
 
 stemmer = PorterStemmer()
+
+os.makedirs("cache", exist_ok=True)
+
+
+
+
+class InvertedIndex:
+
+    def __init__(self):
+        self.index = dict()
+        self.docmap = dict()
+
+    def __add_document(self, doc_id, text):
+        tokens = tokenize(text)
+        for token in tokens:
+            self.index[token] = self.index.get(token, set()) | {doc_id}
+
+    def get_documents(self, term: str):
+        return sorted(self.index[term.lower()])
+
+    def build(self) -> None:
+        with open('./data/movies.json', 'r') as f:
+            data = json.load(f)
+            movies = data["movies"]
+        for m in movies:
+            self.docmap[m['id']] = m
+            self.__add_document(m['id'], f"{m['title']} {m['description']}")
+
+    
+    def save(self):
+        with open("cache/index.pkl", "wb") as f:
+            pickle.dump(self.index, f)
+        with open("cache/docmap.pkl", "wb") as f:
+            pickle.dump(self.docmap, f)
+
+
+    
+
+
+
+
 
 def remove_stop(ls):
     with open('./data/stopwords.txt', 'r') as f:
@@ -48,6 +91,15 @@ def search_movies(query: str):
         for v in result:
             print(v)
 
+def build_index():
+    index = InvertedIndex()
+    index.build()
+    index.save()
+    docs = index.get_documents("merida")
+    print(f"First document for token 'merida' = {docs[0]}")
+
+
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -61,6 +113,9 @@ def main() -> None:
     match args.command:
         case "search":
            search_movies(args.query)
+        case "build":
+           build_index() 
+
         case _:
             parser.print_help()
 
